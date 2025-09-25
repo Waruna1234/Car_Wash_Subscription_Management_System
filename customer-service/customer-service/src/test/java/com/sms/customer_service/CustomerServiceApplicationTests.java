2c9e9ff56482084ca7ef98498da5145390e68d9e
waruna <warunaselaka1994@gmail.com>
2025-09-25 12:44:57 +0530

    add curtomer service

---
+++ customer-service/customer-service/src/test/java/com/sms/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
